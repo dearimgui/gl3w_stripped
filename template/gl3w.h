272fa700a9e5a839cf5099b7efae3567982fb7a0
ocornut <omar@miracleworld.net>
2025-12-11 18:29:56 +0100

    Clear function pointers on shutdown.
    For https://github.com/ocornut/imgui/issues/9112

--- template/gl3w.h
+++ template/gl3w.h
@@ -330,6 +330,7 @@ static int parse_version(void)
 }
 
 static void load_procs(GL3WGetProcAddressProc proc);
+static void clear_procs();
 
 int imgl3wInit(void)
 {
@@ -349,6 +350,7 @@ int imgl3wInit2(GL3WGetProcAddressProc proc)
 void imgl3wShutdown(void)
 {
     close_libgl();
+    clear_procs();
 }
 
 int imgl3wIsSupported(int major, int minor)
@@ -372,6 +374,13 @@ static void load_procs(GL3WGetProcAddressProc proc)
     size_t i;
     for (i = 0; i < GL3W_ARRAY_SIZE(proc_names); i++)
         imgl3wProcs.ptr[i] = proc(proc_names[i]);
+}
+
+static void clear_procs()
+{
+    size_t i;
+    for (i = 0; i < GL3W_ARRAY_SIZE(proc_names); i++)
+        imgl3wProcs.ptr[i] = nullptr;
 }
 
 #ifdef __cplusplus
